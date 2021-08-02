1b5f3dd91ec97e52b3f357e69e317d9cf974e8f6
Anastasya Kovalchuck <anastasya.kovalchuck@gmail.com>
2021-08-02 14:38:30 +0300

    Add material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { makeStyles, Typography, IconButton } from '@material-ui/core';
+import { Shop } from '@material-ui/icons';
 
-const App = () => {
-  (
-    <div>HELLO!</div>
-  )
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: 'red',
+  },
+  title: {
+    fontSize: '20px',
+    color: 'blue',
+  },
+  button: {
+    backgroundColor: 'green',
+  },
+  icon: {
+    color: 'pink',
+  },
+});
+
+const App = () =>  {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Typography className={classes.title}>HELLO!</Typography>
+      <IconButton className={classes.button}>
+        <Shop className={classes.icon} />
+      </IconButton>
+    </div>
+  );
 };
 
 export default App;
